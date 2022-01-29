8a64c1e17291b8648956862bf453482641893324
junior lopez <juniorlopez3@live.com>
2022-01-28 23:43:39 -0500

    Error 404

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => (
   <div className="text-center  mt-32 wow animate__animated animate__shakeX">
-    <div className=" text-3xl md:text-9xl dark:text-white "><b>404</b> - Not Found :(</div>
+    <div className=" text-3xl md:text-9xl dark:text-white "><b>404</b> - No Encontrada</div>
 
     <Link to="/react-dinterweb">
       <h1 className="text-4xl mt-52 dark:text-white"> <i class="fas fa-home dark:text-white"></i> Inicio </h1>
